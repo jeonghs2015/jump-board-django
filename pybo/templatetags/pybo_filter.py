e4a0fb0619e255815d005642b80036ef88a5b49f
hsjung2015 <hsjung201558@gmail.com>
2022-08-18 16:13:26 +0900

    질문등록에 마크다운 기능 추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,7 @@
+
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -7,3 +10,8 @@ register = template.Library()
 def sub(value, arg):
     return value - arg
 
+
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
