b4c3aa408311fb231f34769b5010b7d71380c2fe
hsjung2015 <hsjung201558@gmail.com>
2022-08-18 12:02:49 +0900

    게시글에 추천 기능 추가, 게시글 답변에 추천 기능 추가

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -60,3 +60,13 @@ def question_vote(request, question_id):
     else:
         question.voter.add(request.user)
     return redirect('pybo:detail', question_id=question.id)
+
+
+@login_required(login_url='common:login')
+def question_vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user == question.author:
+        messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
+    else:
+        question.voter.add(request.user)
+    return redirect('pybo:detail', question_id=question.id)
